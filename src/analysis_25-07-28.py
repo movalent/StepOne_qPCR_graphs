2fba80dec34c5636a6bd7d6b690a82d9cf526f74
KlaudiaK <github.container417@passmail.com>
2026-05-15 15:42:44 +0100

    Code refractor     * Read threashold from the yaml file     * Combined separate dataframes into one long clean dataframe

--- src/analysis_25-07-28.py
+++ src/analysis_25-07-28.py
@@ -1,131 +1,51 @@
 # Author: KlaudiaK
 # Creation date: 28 July 2025
 
-import yaml
 from datetime import datetime
 from pathlib import Path
+import re
+import yaml
 
-import openpyxl
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import ScalarFormatter
+import openpyxl
+import pandas as pd
 
+# Options for pandas dataframe print
 pd.options.display.width = 0
 pd.options.display.max_columns = 15
 
-# Resolve the path
-MAIN_DIR = Path(__file__).resolve(strict=True).parent.parent
-print(MAIN_DIR)
-INPUT = MAIN_DIR / 'data' / 'input' / 'Results.xls'
-LEGEND = MAIN_DIR / 'data' / 'metadata' / 'Plate_layout.xlsx'
-OUTPUT = MAIN_DIR / 'data' / 'output'
-CONFIG = MAIN_DIR / 'config' / 'config.yaml'
-print(CONFIG)
-
-# Load the config
-with open(CONFIG) as file:
-    config = yaml.safe_load(file)
-
-REF_GENES = config['housekeeping_genes']
-
-print(REF_GENES)
-
-# Create dataframe to hold sample's info
-df_sample_info = pd.DataFrame(columns=['Well', 'Sample_name', 'Color', 'Include'])
-
-# Process the sample name
-df_name_color = pd.read_excel(LEGEND)
-df_name_color.set_index('Unnamed: 0', inplace=True)
-column_names = list(range(1,13))
-
-for row_letter_1, row in df_name_color.iloc[:8,].iterrows():  # Without iloc dataframe spans to the 2nd include-dataframe
-    for col_name_1, cell_value in zip(column_names, row):
-        cell_position = f'{row_letter_1}{col_name_1}'
-        row = pd.DataFrame([{'Well':cell_position, 'Sample_name':cell_value, 'Color': '', 'Include': ''}])
-        df_sample_info = pd.concat([df_sample_info, row])
-
-df_sample_info.dropna(inplace=True)
-
-# Extracting cell color via openpyxl
-workbook = openpyxl.load_workbook(LEGEND)
-sheet = workbook.active
-for row_c in sheet.iter_rows():
-    for cell in row_c:
-        color = cell.fill.start_color.index
-        df_sample_info.loc[df_sample_info['Sample_name'] == cell.value, 'Color'] = f'#{color[2:]}'  # TIP: openpyxl returns color in format where first two hex symbols must be axed to be read by matplotlib
-
-
-df_include_cell = pd.read_excel(LEGEND, skiprows=10)
-df_include_cell.set_index('Unnamed: 0', inplace=True)
-
-for row_letter_2, row in df_include_cell.iterrows():
-    for col_name_2, cell_value in zip(column_names, row):
-        cell_position = f'{row_letter_2}{col_name_2}'
-        df_sample_info.loc[df_sample_info['Well'] == cell_position, 'Include'] = cell_value
-
-# Read experiment date from metadata
-df_data = pd.read_excel(INPUT, sheet_name='Amplification Data')
-experiment_date = datetime.strptime(df_data.iloc[2, 1][:10], '%Y-%m-%d')
-experiment_date_formatted = experiment_date.strftime('%d %b %Y')
-
-# Load amplification data
-df_results = pd.read_excel(INPUT, sheet_name='Amplification Data', skiprows=7)
-detected_targets = df_results['Target Name'].dropna().unique()
-
-# Select if housekeeping is first or second on the target list
-print('Detected amplification targets: ', detected_targets)
-
-for target_name in detected_targets:
-    if target_name in ['ACTB', 'GAPDH', 'RNAseP']:
-        ref_idx = list(detected_targets).index(target_name)
-        ref_status = 1
-        continue
-    goi_prompt = input(f'Is {target_name} a GoI: y/n ')
-    if goi_prompt == 'y':
-        goi_idx = list(detected_targets).index(target_name)
-        goi_status = 1
-        continue
-    ref_prompt = input(f'Is {target_name} a housekeeping: y/n ')
-    if ref_prompt == 'y':
-        ref_idx = list(detected_targets).index(target_name)
-        ref_status = 1
-
-columns_to_keep = df_sample_info[df_sample_info['Include'] == 1.0]['Well']
-columns_to_keep = list(columns_to_keep)
-
-# Separate results based on target name & removing unwanted cells (not to keep)
-# TODO: If result file has only one target, script will crash as list of targets has only one item.
-targets = []
-if goi_status:
-    df_goi = df_results[(df_results['Target Name'] == detected_targets[goi_idx]) & (df_results['Well'].isin(columns_to_keep))]
-    df_goi_piv = df_goi.pivot_table(index='Cycle', columns='Well', values='ΔRn')
-    targets.append((df_goi_piv, detected_targets[goi_idx]))
-if ref_status:
-    df_ref = df_results[(df_results['Target Name'] == detected_targets[ref_idx]) & (df_results['Well'].isin(columns_to_keep))]
-    df_ref_piv = df_ref.pivot_table(index='Cycle', columns='Well', values='ΔRn')
-    targets.append((df_ref_piv, detected_targets[ref_idx]))
-
-# Dictionary that holds sample info
-df_sample_info.set_index('Well', inplace=True)
-
-legend_dict = {}
-for index, row in df_sample_info.iterrows():
-    legend_dict[index] = (row['Sample_name'], row['Color'])
-
 # Create graph
-def create_graph(targets, date):
-    for target, target_name in targets:
-        x_values = target.index[1:]  # Index of dataframe is cycle number for x values
-        threshold = float(input(f'Threshold for {target_name}: '))
-        for well_coordinates, y_values in target.items():  # Column name is well coordinate, column values are y_values for graph
-            color_temp = legend_dict[well_coordinates][1]  # Retrieve well color and label based on the legend and well position
-            label_temp = legend_dict[well_coordinates][0]
-            plt.plot(x_values, y_values[1:], color=color_temp, linewidth=0.75,
-                     label=label_temp if label_temp not in plt.gca().get_legend_handles_labels()[1] else '')
+def create_graph(df, target_names, thresholds, date):
+
+    for target in target_names:
+
+        slice_df = df.loc[(df['Target Name'] == target) &
+                          (df['include'] == 1)
+                          ]
+
+        if slice_df.empty:
+            continue
+
+        targets = slice_df.groupby(by='sample_name')
+
+        threshold = thresholds[target]
+
+        for sample_name, group in targets:
+
+            x_values = group['Cycle']
+            y_values = group['ΔRn']
+
+            plt.plot(x_values, y_values,
+                    color=group['color'].iloc[0],
+                    linewidth=0.75,
+                    label=sample_name if sample_name not in plt.gca().get_legend_handles_labels()[1] else ''
+                    )
             # HINT: https://stackoverflow.com/a/47949224
 
         # Graph style
-        plt.title(f'Amplification plot for {target_name} {date}' , fontname='Cambria', fontsize=16)
+        plt.title(f'Amplification plot for {target} {date}' , fontname='Cambria', fontsize=16)
         plt.xlabel('Cycle', fontname='Cambria', fontsize=14)
         plt.ylabel('ΔRn', fontname='Cambria', fontsize=14)
 
@@ -143,7 +63,76 @@ def create_graph(targets, date):
         handles, labels = zip(* sorted(zip(handles, labels), key=lambda x: x[1]))
 
         plt.legend(handles, labels, fontsize=9, loc='upper left')
-        # TIP: https://www.sqlpey.com/python/top-2-methods-to-control-legend-order-in-matplotlib/
+        # HINT: https://www.sqlpey.com/python/top-2-methods-to-control-legend-order-in-matplotlib/
+
         plt.show()
 
-create_graph(targets, experiment_date_formatted)
+
+# Resolve the file paths
+MAIN_DIR = Path(__file__).resolve(strict=True).parent.parent
+INPUT = MAIN_DIR / 'data' / 'input' / 'Results.xls'
+LEGEND = MAIN_DIR / 'data' / 'metadata' / 'Plate_layout.xlsx'
+OUTPUT = MAIN_DIR / 'data' / 'output'
+CONFIG = MAIN_DIR / 'config' / 'config.yaml'
+
+# Load the config
+with open(CONFIG) as file:
+    config = yaml.safe_load(file)
+
+    REF_GENES = config['housekeeping_genes']
+    REF_GENES = [x.lower() for x in REF_GENES]
+
+    target_thresholds = config['thresholds']
+
+# Load data
+raw_data = pd.read_excel(INPUT)
+mapping_data = pd.read_excel(LEGEND)
+
+# Experimental date
+experiment_date = re.sub(r'\s?(AM|PM|BST)', '', raw_data.iloc[2, 1])
+experiment_date = pd.to_datetime(experiment_date)
+experiment_date_formatted = experiment_date.strftime('%d %b %Y')
+
+# Pre-prepare the raw data dataframe
+raw_data = raw_data.iloc[6:,]
+raw_data = raw_data.rename(columns=raw_data.iloc[0]).iloc[1:]
+raw_data = raw_data.set_index('Well')
+
+# Load information if well should be included
+mapping_data = pd.read_excel(LEGEND)
+to_include = mapping_data.iloc[10:18, :13]
+to_include = to_include.set_index(to_include.columns[0])
+to_include_long = to_include.stack()
+
+for (row_letter, col_idx), value in to_include_long.items():
+    cell_coord = f'{row_letter}{col_idx}'
+    raw_data.loc[cell_coord, 'include'] = value
+
+# Load name and color metadata
+workbook = openpyxl.load_workbook(LEGEND)
+sheet = workbook.active
+
+for row in sheet.iter_rows(min_row=2, max_row=9):
+    row_letter = row[0].value
+
+    for col_idx, cell in enumerate(row[1:], start=1):
+        cell_coord = f'{row_letter}{col_idx}'
+        value = cell.value
+
+        fill = cell.fill
+        color = None
+
+        # Get fill color
+        if fill.patternType == 'solid':
+            fg = fill.fgColor
+
+            if fg.type == 'rgb' and fg.rgb is not None:
+                color = fg.rgb
+                color = f'#{color[-6:]}'  # Openpyxl returns ARGB and matplotlib accepts RGB
+
+        raw_data.loc[cell_coord, 'sample_name'] = value
+        raw_data.loc[cell_coord, 'color'] = color
+
+target_names = raw_data['Target Name'].dropna().unique()
+
+create_graph(raw_data, target_names, target_thresholds, experiment_date_formatted)
