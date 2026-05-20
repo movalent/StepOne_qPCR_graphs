6682f8f2cd8095f8cec85980c882020245d7377e
KlaudiaK <github.container417@passmail.com>
2026-05-20 16:51:11 +0100

    Refactor: clean up code across main, plotting, and preprocessing modules
    * Remove redundant imports
    * Replace plt usage with fig, ax pattern in plotting module
    * Align function argument indentation

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 # Author: KlaudiaK
 # Creation date: 28 July 2025
 
-import pandas as pd
-
 import file_utils
 import preprocessing
 import plotting
@@ -18,14 +16,14 @@ def main() -> None:
 
     # Load raw data and plate mapping
     raw_data, mapping_data = file_utils.load_raw_data(data_paths['INPUT'], data_paths['LEGEND'])
-    openpyxl_sheet = file_utils.load_plate_layout(data_paths['LEGEND'])
+    openpyxl_sheet = file_utils.load_plate_layout(data_paths['LEGEND'])  # Pandas does not support extracting color
 
     # Process the data
     experiment_date_formatted = preprocessing.extract_date(raw_data)
     raw_data = preprocessing.preprocess_raw_data(raw_data)
     raw_data = preprocessing.preprocess_mapping_data(mapping_data, raw_data)
 
-    raw_data, target_names,  = preprocessing.preprocess_name_color(openpyxl_sheet, raw_data)
+    raw_data, target_names  = preprocessing.preprocess_name_color(openpyxl_sheet, raw_data)
     # print(raw_data)
 
     # Generate and save the plots
--- src/plotting.py
+++ src/plotting.py
@@ -65,50 +65,65 @@ def create_amplification_plots(
         try:
             threshold = thresholds[target]
         except KeyError:
-            threshold = input(f'No threshold specified for {target}. Input the threshold manually, or add to the configuration file and restart the script: ')
+            threshold = float(input(f'No threshold specified for {target}. Input the threshold manually, or add to the configuration file and restart the script: '))
+
+        fig, ax = plt.subplots()
 
         for sample_name, group in targets:
 
             x_values = group['Cycle']
             y_values = group['ΔRn']
 
-            plt.plot(x_values, y_values,
-                    color=group['color'].iloc[0],
-                    linewidth=GRAPH_LINEWIDTH,
-                    label=sample_name if sample_name not in plt.gca().get_legend_handles_labels()[1] else ''
-                    )
+            ax.plot(
+                x_values,
+                y_values,
+                color=group['color'].iloc[0],
+                linewidth=GRAPH_LINEWIDTH,
+                label=sample_name if sample_name not in plt.gca().get_legend_handles_labels()[1] else ''
+                )
             # HINT: https://stackoverflow.com/a/47949224
 
         # Graph style
-        plt.title(f'Amplification plot for {target} {date}' , fontname=GRAPH_FONTNAME, fontsize=TITLE_FONTSIZE)
-        plt.xlabel('Cycle', fontname=GRAPH_FONTNAME, fontsize=AXIS_FONTSIZE)
-        plt.ylabel('ΔRn', fontname=GRAPH_FONTNAME, fontsize=AXIS_FONTSIZE)
+        ax.set_title(
+            f'Amplification plot for {target} {date}',
+            fontname=GRAPH_FONTNAME,
+            fontsize=TITLE_FONTSIZE
+            )
+        ax.set_xlabel('Cycle', fontname=GRAPH_FONTNAME, fontsize=AXIS_FONTSIZE)
+        ax.set_ylabel('ΔRn', fontname=GRAPH_FONTNAME, fontsize=AXIS_FONTSIZE)
 
-        plt.grid(True, color='grey', axis='y')
-        plt.hlines(y=threshold, xmin=DISP_CYCLE_START, xmax=DISP_CYCLE_END,
-                   color=THRESHOLD_LINE_COL, linestyles=THRESHOLD_LINE_STYLE, label='Threshold'
-                   )
+        ax.grid(True, color='grey', axis='y')
+        ax.hlines(
+            y=threshold,
+            xmin=DISP_CYCLE_START,
+            xmax=DISP_CYCLE_END,
+            color=THRESHOLD_LINE_COL,
+            linestyles=THRESHOLD_LINE_STYLE,
+            label='Threshold'
+            )
 
-        plt.xlim([DISP_CYCLE_START, DISP_CYCLE_END])
-        plt.gca().xaxis.set_major_locator(plt.MultipleLocator(DISP_CYCLE_INTERVAL))
+        ax.set_xlim([DISP_CYCLE_START, DISP_CYCLE_END])
+        ax.xaxis.set_major_locator(plt.MultipleLocator(DISP_CYCLE_INTERVAL))
 
-        plt.ylim([DISP_RN_MIN, DISP_RN_MAX])
-        plt.yscale('log')
-        plt.gca().yaxis.set_major_formatter(ScalarFormatter())
+        ax.set_ylim([DISP_RN_MIN, DISP_RN_MAX])
+        ax.set_yscale('log')
+        ax.yaxis.set_major_formatter(ScalarFormatter())
 
         handles, labels = plt.gca().get_legend_handles_labels()
         handles, labels = zip(* sorted(zip(handles, labels), key=lambda x: x[1]))
 
-        plt.legend(handles, labels, fontsize=LEGEND_FONTSIZE, loc='upper left')
+        ax.legend(handles, labels, fontsize=LEGEND_FONTSIZE, loc='upper left')
         # HINT: https://www.sqlpey.com/python/top-2-methods-to-control-legend-order-in-matplotlib/
 
         now = datetime.now()
         fmt_datetime = now.strftime('%Y-%m-%d %H-%M-%S')
 
         for file_type in OUTPUT_FORMATS :
-            plt.savefig(data_paths['OUTPUT'] / f'{date}_{target}_{fmt_datetime}.{file_type}',
-                        transparent=plot_prop['transparent'],
-                        dpi=plot_prop['dpi']
-                        )
+            plt.savefig(
+                data_paths['OUTPUT'] / f'{date}_{target}_{fmt_datetime}.{file_type}',
+                transparent=plot_prop['transparent'],
+                dpi=plot_prop['dpi']
+                )
+
         plt.close()
 
