2bd4ab7d87b3071cc48c1f54cb46b5e849c6201b
KlaudiaK <github.container417@passmail.com>
2026-06-02 14:20:11 +0100

    Add basic logger to main.py module

--- src/main.py
+++ src/main.py
@@ -1,24 +1,37 @@
 # Author: KlaudiaK
 # Creation date: 28 July 2025
 
+import logging
+
 import file_utils
 import preprocessing
 import plotting
 from paths import CONFIG, create_paths
 
+logger = logging.getLogger(__name__)
 
 def main() -> None:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s | %(levelname)s | %(name)s | %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+
     # Load configuation
+    logger.info('Loading configuration')
     target_thresholds, ref_genes, plot_properties, results_file_name = file_utils.load_config(CONFIG)
 
     # Resolve paths
+    logger.info('Resolving paths')
     data_paths = create_paths(results_file_name)
 
     # Load raw data and plate mapping
+    logger.info('Loading raw data')
     raw_data, mapping_data = file_utils.load_raw_data(data_paths['INPUT'], data_paths['LEGEND'])
     openpyxl_sheet = file_utils.load_plate_layout(data_paths['LEGEND'])  # Pandas does not support extracting color
 
     # Process the data
+    logger.info('Preprocessing')
     experiment_date_formatted = preprocessing.extract_date(raw_data)
     raw_data = preprocessing.preprocess_raw_data(raw_data)
     raw_data = preprocessing.preprocess_mapping_data(mapping_data, raw_data)
@@ -27,6 +40,7 @@ def main() -> None:
     # print(raw_data)
 
     # Generate and save the plots
+    logger.info('Creating plots (%d targets)', len(target_names))
     plotting.create_amplification_plots(raw_data, target_names,
                           target_thresholds, plot_properties,
                           experiment_date_formatted, data_paths)
